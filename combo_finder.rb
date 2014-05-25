5f9ab6370b06ddaea99f41390fb1e00a5236d23e
Mark Richman <mbrichman@gmail.com>
2014-05-25 17:12:20 -0500

    primary solution solved

--- combo_finder.rb
+++ combo_finder.rb
@@ -1,6 +1,27 @@
-#!/usr/bin/ruby
+#!/Users/markrichman/.rbenv/shims/ruby
 
 require_relative 'lib/permutationator'
-answer = Permutationator.new([215,275,335,355,420,580], 1505)
+require_relative 'lib/file_parser'
+
+@menu_hash = {}
+File.open(ARGV[0]).each_with_index do |line, i|
+  if i == 0
+  @target = Permutationator.currency_to_number(line)
+  else
+    menu_item = line.split(',')
+    @price = Permutationator.currency_to_number(menu_item[1])
+    @menu_hash[@price] = menu_item[0]
+  end
+end
+
+answer = Permutationator.new(@menu_hash.keys, @target)
+puts @target
 combinations = answer.combos
-puts answer.evaluate(combinations)
+
+solution = answer.evaluate(combinations)
+if solution == false
+  puts "Sorry! Looks like there are no combinations that match the provided price."
+else
+  choices = solution.map {|s| @menu_hash[s]}.join(', ')
+  puts "Yay! You should order the following: #{choices} to equal $#{sprintf( "%0.02f", @target.to_f / 100)}"
+end
--- lib/permutationator.rb
+++ lib/permutationator.rb
@@ -1,8 +1,13 @@
 #!/usr/bin/ruby
+require 'bigdecimal'
 
 class Permutationator
 
   attr_accessor :combos
+
+  def self.currency_to_number(string)
+    (BigDecimal(string.scan(/(\d+[.]\d+)/).flatten.first) * 100).to_i
+  end
 
   def initialize(ary, target)
     @target = target
@@ -13,32 +18,25 @@ class Permutationator
   end
 
   def evaluate(combinations)
-    combinations.each do |c|
-      if sum_values(c) == @target
-        return "Yay! #{c} is what you're looking for!"
-        break
-      else
-        return "Guess you're hungry! There is no valid combination"
-      end
-    end
+    combinations.size > 0 ? combinations.first : false
   end
 
   private
 
-    def sum_values(ary)
-      ary.inject(:+)
-    end
+  def sum_values(ary)
+    ary.inject(:+)
+  end
 
-    def gather_combinations(ary, size, target)
-      raise TypeError "Input must be an array" unless ary.is_a?(Array)
-      return @combinations if size == 0
-      @combinations << ary.repeated_permutation(size).to_a.reject {|a| sum_values(a) > target}
-      gather_combinations(ary, size - 1, target)
-    end
+  def gather_combinations(ary, size, target)
+    raise TypeError "Input must be an array" unless ary.is_a?(Array)
+    return @combinations if size == 0
+    @combinations << ary.repeated_permutation(size).to_a.select {|a| sum_values(a) == target}
+    gather_combinations(ary, size - 1, target)
+  end
 
-    def no_permutations(ary, target)
-      prices = ary.sort {|a,b| a<=>b}
-      target / prices[0]
-    end
+  def no_permutations(ary, target)
+    prices = ary.sort {|a,b| a<=>b}
+    target / prices[0]
+  end
 
 end
