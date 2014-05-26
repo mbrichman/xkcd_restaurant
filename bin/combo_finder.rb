93c2e26047adf36bf2f0b1d206bcbd193ce7d648
Mark Richman <mbrichman@gmail.com>
2014-05-26 16:00:50 -0500

    clear screen

--- bin/combo_finder.rb
+++ bin/combo_finder.rb
@@ -4,10 +4,12 @@ require_relative '../lib/permutationator'
 require_relative '../lib/parser'
 
 raise ArgumentError.new "You must include a filename as an argument" if ARGV.size == 0
-
+puts "\e[H\e[2J"
+puts "Working..."
 menu = Parser.new(ARGV[0])
 target_price = menu.target
 menu_hash = menu.item_hash
 
 answer = Permutationator.new(menu_hash, target_price)
+puts "\e[H\e[2J"
 puts answer.solution
